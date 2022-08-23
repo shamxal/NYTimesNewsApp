9757e111bfcc267fb49490c8eaf41dfac0ebe688
Shamkhal Guliyev <shamxal.guliyev91@gmail.com>
2022-08-23 20:24:21 +0400

    url session codes deleted

--- NYTimesNewsApp/Network/Core/NetworkManager.swift
+++ NYTimesNewsApp/Network/Core/NetworkManager.swift
@@ -13,24 +13,6 @@ class NetworkManager {
     static let shared = NetworkManager()
     
     func request<T: Codable>(type: T.Type, url: String, method: HTTPMethod, completion: @escaping((Result<T, ErrorTypes>)->())) {
-//        let session = URLSession.shared
-//        if let url = URL(string: url) {
-//            var request = URLRequest(url: url)
-//            request.httpMethod = method.rawValue
-//
-//            let dataTask = session.dataTask(with: request) { data, response, error in
-//                if let _ = error {
-//                    completion(.failure(.generalError))
-//                } else if let data = data {
-//                    self.handleResponse(data: data) { response in
-//                        completion(response)
-//                    }
-//                } else {
-//                    completion(.failure(.invalidURL))C
-//                }
-//            }
-//            dataTask.resume()
-//        }
         
 //        AF.request(url, method: method).responseDecodable(of: T.self) { response in
 //            switch response.result {
